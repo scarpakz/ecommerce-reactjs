08313cd1b16997026be118bd400dcd8de35f7d38
Jason Javier <jasonjjavier@gmail.com>
2024-11-26 01:05:56 -0400

    added hot deals

---
+++ src/components/HotDeals.js
@@ -0,0 +1,43 @@
+import { 
+    Container,
+    Row,
+    Col
+} from "react-bootstrap"
+import shoes from '../assets/hot-deals/shoes.png'
+import kids from '../assets/hot-deals/kids.png'
+import woman from '../assets/hot-deals/woman.png'
+
+export const HotDeals = () => {
+    return (
+        <>
+            <Container className="c-hotdeals">
+                <Row>
+                    <Col className="c-banner-title">
+                        <h1>HOT DEALS RIGHT NOW</h1>
+                        <h4>DEALS TOO GOOD TO MISS</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className="c-card-relative">
+                            <img src={shoes} alt="shoes"/>
+                            <span>SHOES UNDER $75</span>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="c-card-relative">
+                            <img src={woman} alt="woman"/>
+                            <span>FALL DISCOUNTS</span>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="c-card-relative">
+                            <img src={kids} alt="kids"/>
+                            <span>KIDS WEAR</span>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,13 @@
 import { NavBar } from "../components/NavBar"
 import { Header } from "../components/Header"
+import { HotDeals } from "../components/HotDeals"
+
 export const HomePage = () => {
     return (
         <>
             <NavBar />
             <Header />
+            <HotDeals />
         </>
     ) 
 }
