1a54e0ec7cb4a9c65847e6f8dd97620e508a97f4
Jason Javier <jasonjjavier@gmail.com>
2024-11-26 22:22:02 -0400

    added two image links for everyone's list component

--- src/components/EveryoneList.js
+++ src/components/EveryoneList.js
@@ -3,6 +3,8 @@ import {
     Row,
     Col
 } from "react-bootstrap"
+import wintershoes from '../assets/wintershoes.png'
+import womandress from '../assets/womandress.png'
 
 export const EveryoneList = () => {
     return (
@@ -12,6 +14,16 @@ export const EveryoneList = () => {
                     <Col className="c-banner-title">
                         <h1>ON EVERYONE'S LIST</h1>
                         <h4>OUR TOP CHOICES THIS YEAR</h4>
+                    </Col>
+                </Row>
+                <Row className="c-everyonelist-links">
+                    <Col className="c-everyonelist-item">
+                        <img src={wintershoes}/>
+                        <span>WINTER CLASSICS</span>
+                    </Col>
+                    <Col className="c-everyonelist-item">
+                        <img src={womandress}/>
+                        <span>FAVORITES</span>
                     </Col>
                 </Row>
             </Container>
